2f159b0430d1f3e888065fbe8c211d074dd795d8
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2022-05-05 16:06:53 -0700

    Add: User model

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongose");
+const Schema =mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email: {type: String, unique: true, require: true },
+    password: {type: String, require: true },
+    address: {type: Schema.Types.ObjectId, ref: "Address" },
+});
+
+module.exports = mongoose.model("User", UserSchema);
