1864b8c4ce2b15aadb4b85213def180ea8a43127
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2022-05-06 22:03:52 -0700

    ADD: Add product frontemd

--- admin/components/Header.vue
+++ admin/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
- <div>
-  <b-navbar toggleable="lg" class="amazon-bg">
+  <div>
+    <b-navbar toggleable="lg" class="amazon-bg">
       <b-navbar-brand href="#">Amason Admin</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <NuxtLink to="/product" class="a-button-buy-again">Add Products</NuxtLink>
+          <NuxtLink to="/product" class="a-button-buy-again"
+            ><font-awesome-icon icon="fa-duotone fa-album-circle-plus" />Add
+            Products</NuxtLink
+          >
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -42,15 +45,12 @@
       </b-collapse>
     </b-navbar>
     <div class="a-spacing-large"></div>
-    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
