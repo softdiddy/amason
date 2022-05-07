2225a4dc5b207c16f40e7d5fcee01fdf416a3d1f
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2022-05-06 17:37:23 -0700

    ADD: Nuxt setup and custom style setup

---
+++ admin/components/Login.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div >
+    <h1>Hello Login</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLogin'
+}
+</script>
---
+++ admin/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a href="" class="a-button-buy-again">Buy Now</a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
